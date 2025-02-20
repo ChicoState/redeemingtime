20f97f98d90800622c361694a226adc597c303b2
trevorleeshelton <tshelton@csuchico.edu>
2025-02-19 19:22:30 -0800

    Server now can deliver a basic api response.

--- rtt_backend/api/views.py
+++ rtt_backend/api/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def invoke(request):
+    return render(request, 'response.json.template')
--- rtt_backend/rtt_backend/settings.py
+++ rtt_backend/rtt_backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR / 'templates'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'rtt_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [ TEMPLATE_DIR ],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
