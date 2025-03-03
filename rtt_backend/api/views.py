562b3715d52aba79654ce56c7a475acc36ba11b9
trevorleeshelton <tshelton@csuchico.edu>
2025-03-03 13:57:13 -0800

    Separated the task handling into its own function and sub-functions.

--- rtt_backend/api/views.py
+++ rtt_backend/api/views.py
@@ -4,12 +4,31 @@ import time
 import secrets
 import json
 
-def err(sc):
+# Simple Error Page
+
+def _err(sc):
     res = http.HttpResponse()
     res.status_code = sc
     res.content = '<!DOCTYPE html>\n<html>\n   <head>\n</head>\n<body>\n   <h1>' + str(sc) + ' ' + res.reason_phrase + '</h1>\n</body>\n</html>'
     return res
 
+# Tasks and Task Parser
+
+def _schedule():
+    return {'Sunday': '', 'Monday': '', 'Tuesday': '', 'Wednesday': '', 'Thursday': '', 'Friday': '', 'Saturday': ''}
+
+def _bad():
+    return {'Bad':'Request'}
+
+def _task(cwant):
+    if (cwant == 'Schedule'):
+        # They want a schedule.
+        return _schedule()
+    else:
+        # I don't know what you want.
+        return _bad()
+
+# View as a Whole
 # REMOVE EXEMPTION FOR PRODUCTION #
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
@@ -42,39 +61,29 @@ def invoke(request):
                 # Only actually create a new user if the request is good.
             else: # Authenticate existing user.
                 if 'User' not in dictIn or 'Pass' not in dictIn: # Incomplete Credentials
-                    return err(401)
+                    return _err(401)
                 instF = ClientData.objects.filter(username=dictIn['User'])
                 if (len(instF) == 0): # User is incorrect.
-                    return err(401)
+                    return _err(401)
                 inst = instF[0]
                 if (inst.check_password(dictIn['Pass']) != True): # Password is incorrect.
-                    return err(401)
+                    return _err(401)
 		        # Record that this user has done an action just now.
                 inst.lastRequest = time.time_ns()
                 inst.save(force_update=True)
             # Do we have a 'Needs' field?
             if 'Needs' not in dictIn:
-                return err(422)
+                return _err(422)
             # What does the client want?
-            needs = dictIn['Needs']
-            if (needs == 'Schedule'):
-                # They want a schedule.
-                dictMain['Sunday'] = '<Activity>'
-                dictMain['Monday'] = '<Activity>'
-                dictMain['Tuesday'] = '<Activity>'
-                dictMain['Wednesday'] = '<Activity>'
-                dictMain['Thursday'] = '<Activity>'
-                dictMain['Friday'] = '<Activity>'
-                dictMain['Saturday'] = '<Activity>'
-            else:
-                # I don't know what you want.
-                return err(422)
+            dictMain = _task(dictIn['Needs'])
+            if (dictMain == _bad()):
+                return _err(422)
         # Request is not in json.
         except json.decoder.JSONDecodeError:
-            return err(406)
+            return _err(406)
     # Only POST.
     else:
-        return err(403)
+        return _err(403)
     # Successful! Now respond (and, if needed, register the new user)!
     if (dictCrd != {}):
         inst = ClientData()
