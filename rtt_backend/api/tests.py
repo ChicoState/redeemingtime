0d9293a47c08bf56dd805fb2f12e37bedd7d3be7
trevorleeshelton <tshelton@csuchico.edu>
2025-03-10 18:32:25 -0700

    Made user credentials client settable, modified models and tests, removed last request time, and neatened view functions.

--- rtt_backend/api/tests.py
+++ rtt_backend/api/tests.py
@@ -1,128 +1,90 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django import http
-from api.models import ClientData
 import json
 
 # We want to verify...
 #   A: ...that the server gives the correct responses to different types of requests.
 #   B: ...that the database doesn't add or not add entries inappropriately.
 #   C: ...that the data sent back is correct.
-#   D: ...that last request time is different per saving of that field.
 class RequestTestCase(TestCase):
+    # Testing Credentials
+    USER='Jim'
+    PASS='Banana'
+    def basic(self, expectedRes, expectedDB, reqDict):
+        response = self.client.post('/', reqDict, content_type='application/json')
+        self.assertEqual(response.status_code, expectedRes)
+        self.assertEqual(len(User.objects.all()), expectedDB)
+        user = authenticate(username=RequestTestCase.USER, password=RequestTestCase.PASS)
+        if (expectedDB == 0):
+            self.assertEqual(user, None)
+        else:
+            self.assertNotEqual(user, None)
+    def authenticated(self, expectedRes, reqDict):
+        self.client.post('/', {'Needs':'Register', 'Username': RequestTestCase.USER, 'Password': RequestTestCase.PASS}, content_type='application/json')
+        response = self.client.post('/', reqDict, content_type='application/json')
+        self.assertEqual(response.status_code, expectedRes)
     def test_get(self):
         # There's No Website
         response = self.client.get('/')
         self.assertEqual(response.status_code, 403)
-        self.assertEqual(len(ClientData.objects.all()), 0)
+        self.assertEqual(len(User.objects.all()), 0)
     def test_emptyPost(self):
         # No Data in Post
         response = self.client.post('/')
         self.assertEqual(response.status_code, 406)
-        self.assertEqual(len(ClientData.objects.all()), 0)
+        self.assertEqual(len(User.objects.all()), 0)
     def test_gibberishPost(self):
         # Data in Post is Garbage
         response = self.client.post('/', {'asdcawwf':'ebfwhwqiubfyfbgeiwfew'})
         self.assertEqual(response.status_code, 406)
-        self.assertEqual(len(ClientData.objects.all()), 0)
+        self.assertEqual(len(User.objects.all()), 0)
     def test_emptyJSONPost(self):
         # Json Data in Post is Empty
-        response = self.client.post('/', {}, content_type='application/json')
-        self.assertEqual(response.status_code, 422)
-        self.assertEqual(len(ClientData.objects.all()), 0)
+        self.basic(422, 0, {})
     def test_gibberishJSONPost(self):
         # Json Data in Post is Garbage
-        response = self.client.post('/', {'ababa':'babab'}, content_type='application/json')
-        self.assertEqual(response.status_code, 422)
-        self.assertEqual(len(ClientData.objects.all()), 0)
+        self.basic(422, 0, {'ababa':'babab'})
     def test_gibberishNeedsJSONPost(self):
-        # Needs in Post is Garbage
-        response = self.client.post('/', {'Needs':'babab'}, content_type='application/json')
-        self.assertEqual(response.status_code, 422)
-        self.assertEqual(len(ClientData.objects.all()), 0)
-    def test_newUserJSONPost(self):
-        # This will occur when a valid need is sent without ANY credentials, indicating a new entity.
-        response = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(ClientData.objects.all()), 1)
-        resJson = json.loads(response.content)
-        data = resJson['Data']
-        self.assertTrue('Sunday' in data)
-        self.assertTrue('Monday' in data)
-        self.assertTrue('Tuesday' in data)
-        self.assertTrue('Wednesday' in data)
-        self.assertTrue('Thursday' in data)
-        self.assertTrue('Friday' in data)
-        self.assertTrue('Saturday' in data)
-        credentials = resJson['Credentials']
-        self.assertEqual(len(credentials['User']), ClientData.CRED_LENGTH)
-        self.assertTrue('Saturday' in data)
+        # Needs in Post is Garbage. Should hit that it's not authenticated.
+        self.basic(401, 0, {'Needs':'babab'})
+    def test_badNewUserJSONPost1(self):
+        # Incomplete Registration
+        self.basic(422, 0, {'Needs':'Register'})
+    def test_badNewUserJSONPost2(self):
+        # Incomplete Registration
+        self.basic(422, 0, {'Needs':'Register', 'Username': RequestTestCase.USER})
+    def test_badNewUserJSONPost3(self):
+        # Incomplete Registration
+        self.basic(422, 0, {'Needs':'Register', 'Password': RequestTestCase.PASS})
+    def test_goodNewUserJSONPost(self):
+        # Successful Registration
+        self.basic(200, 1, {'Needs':'Register', 'Username': RequestTestCase.USER, 'Password': RequestTestCase.PASS})
     def test_incompleteUserJSONPostOne(self):
-        # No Password Provided in Credentials
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        user = json.loads(first.content)['Credentials']['User']
-        response = self.client.post('/', {'Needs':'Schedule', 'User':user}, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        # No Credentials in Authentication
+        self.authenticated(401, {'Needs':'Test', 'Input': {}})
     def test_incompleteUserJSONPostTwo(self):
+        # No Password Provided in Credentials
+        self.authenticated(401, {'Needs':'Test', 'Input': {}, 'Username': RequestTestCase.USER})
+    def test_incompleteUserJSONPostThree(self):
         # No Username Provided in Credentials
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        pswd = json.loads(first.content)['Credentials']['Pass']
-        response = self.client.post('/', {'Needs':'Schedule', 'Pass':pswd}, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        self.authenticated(401, {'Needs':'Test', 'Input': {}, 'Password': RequestTestCase.PASS})
     def test_badUserJSONPostOne(self):
         # Bad Credentials
-        self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        response = self.client.post('/', {'Needs':'Schedule', 'User':'Gibberish', 'Pass':'Gibberish'}, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        self.authenticated(401, {'Needs':'Test', 'Input': {}, 'Username': 'Gibberish', 'Password':'Gibberish'})
     def test_badUserJSONPostTwo(self):
         # Bad Password
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        user = json.loads(first.content)['Credentials']['User']
-        response = self.client.post('/', {'Needs':'Schedule', 'User':user, 'Pass':'Gibberish'}, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        self.authenticated(401, {'Needs':'Test', 'Input': {}, 'Username': 'Gibberish', 'Password': RequestTestCase.PASS})
     def test_badUserJSONPostThree(self):
         # Bad Username
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        pswd = json.loads(first.content)['Credentials']['Pass']
-        response = self.client.post('/', {'Needs':'Schedule', 'User':'Gibberish', 'Pass':pswd}, content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        self.authenticated(401, {'Needs':'Test', 'Input': {}, 'Username': RequestTestCase.USER, 'Password':'Gibberish'})
     def test_existingUserBadJSONPost(self):
-        # Good Credentials, Bad Command
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        user = json.loads(first.content)['Credentials']['User']
-        pswd = json.loads(first.content)['Credentials']['Pass']
-        current = ClientData.objects.filter(username=user)[0]
-        starttime = current.lastRequest
-        response = self.client.post('/', {'Needs':'desdefeas', 'User':user, 'Pass':pswd}, content_type='application/json')
-        self.assertEqual(response.status_code, 422)
-        current = ClientData.objects.filter(username=user)[0]
-        self.assertNotEqual(starttime, current.lastRequest)
-        self.assertEqual(len(ClientData.objects.all()), 1)
-    def test_existingUserGoodJSONPost(self):
-        # Good Credentials, Good Command, Returning User
-        first = self.client.post('/', {'Needs':'Schedule'}, content_type='application/json')
-        user = json.loads(first.content)['Credentials']['User']
-        pswd = json.loads(first.content)['Credentials']['Pass']
-        current = ClientData.objects.filter(username=user)[0]
-        starttime = current.lastRequest
-        response = self.client.post('/', {'Needs':'Schedule', 'User':user, 'Pass':pswd}, content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        resJson = json.loads(response.content)
-        self.assertTrue('Credentials' not in resJson)
-        self.assertTrue('User' not in resJson)
-        self.assertTrue('Pass' not in resJson)
-        data = resJson['Data']
-        self.assertTrue('Sunday' in data)
-        self.assertTrue('Monday' in data)
-        self.assertTrue('Tuesday' in data)
-        self.assertTrue('Wednesday' in data)
-        self.assertTrue('Thursday' in data)
-        self.assertTrue('Friday' in data)
-        self.assertTrue('Saturday' in data)
-        current = ClientData.objects.filter(username=user)[0]
-        self.assertNotEqual(starttime, current.lastRequest)
-        self.assertEqual(len(ClientData.objects.all()), 1)
+        # Good Credentials, Bad Input
+        self.authenticated(422, {'Needs':'Test', 'Username': RequestTestCase.USER, 'Password': RequestTestCase.PASS})
+    def test_existingUserGoodJSONPostOne(self):
+        # Good Credentials, Bad Input
+        self.authenticated(200, {'Needs':'Test', 'Input': 'Something', 'Username': RequestTestCase.USER, 'Password': RequestTestCase.PASS})
+    def test_existingUserGoodJSONPostTwo(self):
+        # Good Credentials, Good Input
+        self.authenticated(200, {'Needs':'Test', 'Input': {'a': 'b'}, 'Username': RequestTestCase.USER, 'Password': RequestTestCase.PASS})
