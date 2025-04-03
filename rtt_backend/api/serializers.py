db319cdadc9f28672e0e50b29b79f28f385cd7c0
trevorleeshelton <tshelton@csuchico.edu>
2025-04-02 17:22:28 -0700

    Explicit functionality for a user to create and list goals has been implemented.

--- rtt_backend/api/serializers.py
+++ rtt_backend/api/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from api import models
 
-class GoalSerializer(serializers.ModelSerializer):
+class GoalWriteSerializer(serializers.ModelSerializer):
     class Meta():
         model = models.Goal
+        fields = ['owner', 'name', 'description']
+
+class GoalReadSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = models.Goal
+        fields = ['name', 'description']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta():
